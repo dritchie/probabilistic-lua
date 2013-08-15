f8ca6420e975e8712cf398ac3debc6f37ed4fc4f
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2013-08-15 11:33:29 -0700

    Using an example with variables coupled via factors demonstrates the effectiveness of HMC-based methods over random walks. T3 seems to be doing pretty well, too.

--- test/expectation.lua
+++ test/expectation.lua
@@ -14,12 +14,22 @@ local means = {-1.0, 0.5, -0.2, 0.7}
 local sds = {0.1, 1.2, 0.5, 0.3}
 local uniformPrior_min = -1.0
 local uniformPrior_max = 1.0
--- local gaussianFactor_meanPriorMean = 0.0
--- local gaussianFactor_meanPriorSD = 2.0
--- local gaussianFactor_sdPriorMin = 0.1
--- local gaussianFactor_sdPriorMax = 2.0
+local targetSum = -0.25
+local targetSumSD = 0.01
 
-local function generate()
+local function gaussianPrior()
+	local dim = uniformDraw(dims, {isStructural=true})
+	local accum = 0.0
+	for i=1,dim do
+		local mean = means[i]
+		local sd = sds[i]
+		local x = gaussian({mean, sd})
+		accum = accum + x
+	end
+	return accum
+end
+
+local function uniformPriorGaussianFactor()
 	local dim = uniformDraw(dims, {isStructural=true})
 	local accum = 0.0
 	for i=1,dim do
@@ -27,27 +37,41 @@ local function generate()
 		local sd = sds[i]
 		local x = uniform({uniformPrior_min, uniformPrior_max})
 		factor(random.gaussian_logprob(x, mean, sd))
-		-- local x = gaussian({mean, sd})
 		accum = accum + x
 	end
 	return accum
 end
 
+local function uniformPriorTargetSumFactor()
+	local dim = uniformDraw(dims, {isStructural=true})
+	local accum = 0.0
+	for i=1,dim do
+		local x = uniform({uniformPrior_min, uniformPrior_max})
+		accum = accum + x
+	end
+	local distFromTarget = accum - targetSum
+	factor(-distFromTarget*distFromTarget/targetSumSD)
+	return accum
+end
+
+
+-- Which test are we doing?
+local generate = uniformPriorTargetSumFactor
 
 
 local groundTruthParams = 
 {
 	numsamps = 1000,
-	lag = 10,
-	runs = 5,
+	lag = 1,
+	runs = 1,
 	burnin = 100,
 	jumpFreq = 0.25
 }
 local testParams = 
 {
 	numsamps = 1000,
-	lag = 10,
-	runs = 5,
+	lag = 1,
+	runs = 1,
 	burnin = 100,
 	jumpFreq = 0.25
 }
@@ -58,30 +82,27 @@ function estimateExpectation(name, computation, sampler, params)
 	print(string.format("%s: %g", name, est))
 end
 
--- The following numbers (in comments) are all for the case where we have gaussian
--- priors and no factors.
 
--- I think the analytical ground truth is
---   (1/2)(-1.0 + 0.5) + (1/2)(-1.0 + 0.5 + -0.2 + 0.7)
---    = (1/2)(-0.5) + (1/2)(0)
---    = -0.25 
 
 math.randomseed(os.time())
 
-local verbose=false
+local verbose=true
 
 print("GROUND TRUTH:\n---------------")
 
--- Plain old reversible jump MH estimates it around -0.238
-estimateExpectation("RJMH", generate, LARJTraceMH, util.jointables(groundTruthParams, {verbose=verbose}))
+--estimateExpectation("RJMH", generate, LARJTraceMH, util.jointables(groundTruthParams, {verbose=verbose}))
+
 
 print("\nESTIMATES:\n---------------")
 
--- Reversible jump LMC gives us about -0.236
 --estimateExpectation("RJLMC", generate, LARJLMC, util.jointables(testParams, {verbose=verbose}))
 
--- LARJ LMC gives us about -0.26
-estimateExpectation("LARJLMC", generate, LARJLMC, util.jointables(testParams, {annealIntervals=100, globalTempMult=0.99, verbose=verbose}))
+--estimateExpectation("LARJLMC", generate, LARJLMC, util.jointables(testParams, {annealIntervals=100, globalTempMult=0.99, verbose=verbose}))
 
--- T3 gives us about -0.263
---estimateExpectation("T3HMC", generate, T3HMC, util.jointables(testParams, {numT3Steps=100, T3StepSize=0.05, globalTempMult=0.99, verbose=verbose}))
+estimateExpectation("T3HMC", generate, T3HMC, util.jointables(testParams, {numT3Steps=100, T3StepSize=0.005, globalTempMult=0.99, verbose=verbose}))
+
+
+
+
+
+
